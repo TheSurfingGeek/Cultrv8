040021439d90eb84d84e999ac3ac38cea6f3e23a
ShannonG <thesurfinggeek@gmail.com>
2018-11-04 22:42:52 +1300

    Create insert statement for form submit.

--- anchors_oars.php
+++ anchors_oars.php
@@ -76,31 +76,18 @@
 		
 				// Output variables for debugging purposes         // 
 						      $mySession = session_id();
-						//	   print $mySession;
+							   
+								print $mySession;
+								print $inputOarEntry1;
+								print $inputOarEntry2;
+								print $inputOarEntry3;
+								print $inputAnchorEntry1;
+							    print $inputAnchorEntry2;
+								print $inputAnchorEntry3;
 								
-						//		print $carSelectedIdPassed;
-						//			print $responseWord1;
-						//				print $selectedAgreeDisagree1;
-						//					print $inputWords1;
-											
-						//			print $responseWord2;
-						//				print $selectedAgreeDisagree2;
-						//					print $inputWords2;
-										
-						//			print $responseWord3;	
-						//				print $selectedAgreeDisagree3;
-						//					print $inputWords3;
-											
-						//			print $responseWord4;		
-						//				print $selectedAgreeDisagree4;
-						//					print $inputWords4;
-									
-						//			print $responseWord5;
-						//				print $selectedAgreeDisagree5;
-						//					print $inputWords5;
-											
 						// Do insert into database record                         //					
 						//Connect to the database using the PDO conection method  //
+						
 						try {
 							require('./dbscripts/cultrv8_mysql_PDO_connect.php'); 
 				
@@ -108,68 +95,39 @@
 								$dbh = new PDO('mysql:host='. DB_HOST .';dbname=' .DB_NAME, DB_USER, DB_PASSWORD);
 								$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	
-									$carInsertStmt = $dbh->prepare("INSERT INTO car_response_word_association_response 
-																	(
-																		session_id,
-																		car_id,
-																		response_word1,
-																		response_word2,
-																		response_word3,
-																		response_word4,
-																		response_word5,
-																		response1_alternative,
-																		response2_alternative,
-																		response3_alternative,
-																		response4_alternative,
-																		response5_alternative,
-																		word1_response,
-																		word2_response,
-																		word3_response,
-																		word4_response,
-																		word5_response,
-																		time_stamp
-																	)
-																	VALUES (
-																				:v_session_id,
-																				:v_car_id,
-																				:v_response_word1,
-																				:v_response_word2,
-																				:v_response_word3,
-																				:v_response_word4,
-																				:v_response_word5,
-																				:v_response1_alternative,
-																				:v_response2_alternative,
-																				:v_response3_alternative,
-																				:v_response4_alternative,
-																				:v_response5_alternative,
-																				:v_word1_response,
-																				:v_word2_response,
-																				:v_word3_response,
-																				:v_word4_response,
-																				:v_word5_response,
-																				NOW()
-																			)");
+									$anchors_Oars_InsertStmt = $dbh->prepare("INSERT INTO anchors_oars_response
+																				(
+																					session_id,
+																					oars_response1,
+																					oars_response2,
+																					oars_response3,
+																					anchors_response1,
+																					anchors_response2,
+																					anchors_response3,
+																					time_stamp
+																				)
+																				VALUES
+																				(
+																					:v_session_id,
+																					:v_oars_response1,
+																					:v_oars_response2,
+																					:v_oars_response3,
+																					:v_anchors_response1,
+																					:v_anchors_response2,
+																					:v_anchors_response3,
+																					NOW()
+																				)");
 																				
-																				$carInsertStmt->bindParam(':v_session_id', $mySession);
-																				$carInsertStmt->bindParam(':v_car_id', $carSelectedIdPassed);
-																				$carInsertStmt->bindParam(':v_response_word1', $responseWord1);
-																				$carInsertStmt->bindParam(':v_response_word2', $responseWord2);
-																				$carInsertStmt->bindParam(':v_response_word3', $responseWord3);
-																				$carInsertStmt->bindParam(':v_response_word4', $responseWord4);
-																				$carInsertStmt->bindParam(':v_response_word5', $responseWord5);
-																				$carInsertStmt->bindParam(':v_response1_alternative', $inputWords1);
-																				$carInsertStmt->bindParam(':v_response2_alternative', $inputWords2);
-																				$carInsertStmt->bindParam(':v_response3_alternative', $inputWords3);
-																				$carInsertStmt->bindParam(':v_response4_alternative', $inputWords4);
-																				$carInsertStmt->bindParam(':v_response5_alternative', $inputWords5);
-																				$carInsertStmt->bindParam(':v_word1_response', $selectedAgreeDisagree1);
-																				$carInsertStmt->bindParam(':v_word2_response', $selectedAgreeDisagree2);
-																				$carInsertStmt->bindParam(':v_word3_response', $selectedAgreeDisagree3);
-																				$carInsertStmt->bindParam(':v_word4_response', $selectedAgreeDisagree4);
-																				$carInsertStmt->bindParam(':v_word5_response', $selectedAgreeDisagree5);
+																					$anchors_Oars_InsertStmt->bindParam(':v_session_id', $mySession);
+																					$anchors_Oars_InsertStmt->bindParam(':v_oars_response1', $inputOarEntry1);
+																					$anchors_Oars_InsertStmt->bindParam(':v_oars_response2', $inputOarEntry2);
+																					$anchors_Oars_InsertStmt->bindParam(':v_oars_response3', $inputOarEntry3);
+																					$anchors_Oars_InsertStmt->bindParam(':v_anchors_response1', $inputAnchorEntry1);
+																					$anchors_Oars_InsertStmt->bindParam(':v_anchors_response2', $inputAnchorEntry2);
+																					$anchors_Oars_InsertStmt->bindParam(':v_anchors_response3', $inputAnchorEntry3);
 											
 																					// Now run the insert query
-																						$carInsertStmt->execute();
+																						$anchors_Oars_InsertStmt->execute();
 							}
 								catch(PDOException $e)   {
 										Print ' <div class="alert alert-danger" role="alert">
